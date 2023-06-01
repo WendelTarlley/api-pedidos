1da2d4ea5a2fc6d19ffca6585ca310620a4c1a66
wendeltarlley+github@gmail.com <wendeltarlley+github@gmail.com>
2023-06-01 13:37:55 -0300

    Renomeando aplicação

--- src/main/java/com/devtarlley/pedidos/ApiPedidosApplication.java
+++ src/main/java/com/devtarlley/pedidos/ApiPedidosApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PedidosApplication {
+public class ApiPedidosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PedidosApplication.class, args);
+		SpringApplication.run(ApiPedidosApplication.class, args);
 	}
 
 }
